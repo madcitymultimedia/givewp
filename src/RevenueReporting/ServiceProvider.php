35f2075fbe6d4271c191746f25bc9659c574f9ab
Kyle Johnson <kbjohnson90+github@gmail.com>
2020-10-27 16:34:48 -0400

    chore: Scaffold Revenue Reporting ServiceProvider.

---
+++ src/RevenueReporting/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Give\RevenueReporting;
+
+use Give\Helpers\Hooks;
+
+class ServiceProvider implements \Give\ServiceProviders\ServiceProvider {
+	/**
+	 * @inheritDoc
+	 *
+	 * @since UNRELEASED
+	 */
+	public function register() {
+		// ...
+		$register = true;
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @since UNRELEASED
+	 */
+	public function boot() {
+		// ...
+		$boot = true;
+	}
+}
